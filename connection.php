27f0f148dd4d467a8766147dde4c061ca0a2f98e
DarkyZ <92459505+darkyzowo@users.noreply.github.com>
2023-02-16 20:16:33 +0500

    ya

--- connection.php
+++ connection.php
@@ -1,16 +1,16 @@
 <?php
 
-    $servername="localhost";
-    $username="root";
-    $password="";
-    $dbname="users";
+$servername = "localhost";
+$username = "root";
+$password = "root123";
+$dbname = "users";
 
-    // Create connection
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
-    // Check connection
-    if(!$conn){
-        die("Connection failed: ".mysqli_connect_error());
-    }
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
- ?>
+?>
